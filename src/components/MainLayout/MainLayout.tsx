4c64a79f568ead949463f48012c35e1935fbc08b
uniorunr <gagael23@live.com>
2020-10-24 23:46:54 +0300

    feat: configure serverless auto deploy

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -9,8 +9,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        My Store
+      <Link color="inherit" href="https://github.com/uniorunr">
+        uniorunr
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
