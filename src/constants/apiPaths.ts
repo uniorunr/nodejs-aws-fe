3b33f975ef61e7ad3e74ae0ebf1428e11d208577
uniorunr <gagael23@live.com>
2020-11-16 03:34:15 +0300

    feat: add import service endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://aj7bc2l319.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://74lc2v7o4j.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://aj7bc2l319.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
